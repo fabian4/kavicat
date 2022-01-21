367a9a5612d7c2d80b2bf9a67dc612e12693720a
fabian4 <fabian4@163.com>
2022-01-21 19:28:49 +0800

    customer theme

--- client/client.go
+++ client/client.go
@@ -3,28 +3,42 @@ package client
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"time"
+	"github.com/fabian4/kavicat/myTheme"
+	"log"
 )
 
 func New() {
 	a := app.New()
+	a.Settings().SetTheme(myTheme.NewTheme())
 	w := a.NewWindow("kavicat")
 	w.Resize(fyne.NewSize(1000, 600))
+	w.CenterOnScreen()
 
-	clock := widget.NewLabel("")
-	w.SetContent(clock)
+	toolbar := widget.NewToolbar(
+		widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
+			log.Println("New document")
+		}),
+		widget.NewToolbarSeparator(),
+		widget.NewToolbarAction(theme.ContentCutIcon(), func() {}),
+		widget.NewToolbarAction(theme.ContentCopyIcon(), func() {}),
+		widget.NewToolbarAction(theme.ContentPasteIcon(), func() {}),
+		widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.HelpIcon(), func() {
+			log.Println("Display help")
+		}),
+	)
 
-	go func() {
-		for range time.Tick(time.Second) {
-			updateTime(clock)
-		}
-	}()
+	left := canvas.NewText("left", theme.ForegroundColor())
+	middle := canvas.NewText("content", theme.ForegroundColor())
+	content := container.New(layout.NewBorderLayout(toolbar, nil, left, nil),
+		toolbar, left, middle)
+
+	w.SetContent(content)
 
 	w.ShowAndRun()
-}
-
-func updateTime(clock *widget.Label) {
-	formatted := time.Now().Format("Time: 03:04:05")
-	clock.SetText(formatted)
 }
