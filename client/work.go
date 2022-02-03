b068f624661d4752f1232e2a42f0ade488305100
fabian4 <fabian4@163.com>
2022-02-03 22:14:34 +0800

    update work space

--- client/work.go
+++ client/work.go
@@ -2,13 +2,56 @@ package client
 
 import (
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"strconv"
 )
 
 func NewWork() fyne.CanvasObject {
 
-	work := canvas.NewText("content", theme.ForegroundColor())
+	//return canvas.NewText("content", theme.ForegroundColor())
 
-	return work
+	return container.NewBorder(newTop(), nil, nil, nil, newKeys())
+}
+
+func newTop() fyne.CanvasObject {
+	return container.NewVBox(widget.NewLabel("================="), widget.NewSeparator())
+}
+
+func newKeys() fyne.CanvasObject {
+	data := make([]string, 1000)
+	for i := range data {
+		data[i] = "Test Item " + strconv.Itoa(i)
+	}
+
+	icon := widget.NewIcon(nil)
+	label := widget.NewLabel("Select An Item From The List")
+	hbox := container.NewHBox(icon, label)
+
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return container.NewHBox(widget.NewIcon(theme.DocumentIcon()), widget.NewLabel("Template Object"))
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*fyne.Container).Objects[1].(*widget.Label).SetText(data[id])
+		},
+	)
+	list.OnSelected = func(id widget.ListItemID) {
+		label.SetText(data[id])
+		icon.SetResource(theme.DocumentIcon())
+	}
+	list.OnUnselected = func(id widget.ListItemID) {
+		label.SetText("Select An Item From The List")
+		icon.SetResource(nil)
+	}
+	list.Select(125)
+
+	split := container.NewHSplit(list, container.NewCenter(hbox))
+	split.Offset = 0.2
+
+	return split
 }
